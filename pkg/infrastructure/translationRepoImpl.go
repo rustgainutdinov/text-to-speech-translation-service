60e8bec9eca22909bf7708dc52c42170afbb5373
Rustam <rustamikc@gmail.com>
2021-06-14 21:00:54 +0300

    запилил store транслита

--- pkg/infrastructure/translationRepoImpl.go
+++ pkg/infrastructure/translationRepoImpl.go
@@ -1,7 +1,6 @@
 package infrastructure
 
 import (
-	"fmt"
 	"github.com/jmoiron/sqlx"
 	"text-to-speech-translation-service/pkg/domain"
 )
@@ -11,8 +10,16 @@ type translationRepo struct {
 }
 
 func (t *translationRepo) Store(translation domain.Translation) error {
-	fmt.Println("translation stored")
-	return nil
+	_, err := t.db.Query(
+		`INSERT INTO transaction (id_transaction, id_user, status, text_to_translate, translated_data)
+		VALUES ($1, $2, $3, $4, $5)
+		ON CONFLICT (id_transaction)
+			DO UPDATE SET id_user           = $2,
+						  status            = $3,
+						  text_to_translate = $4,
+						  translated_data   = $5;`,
+		translation.ID, translation.UserID, translation.Status, translation.Text, translation.SpeechData)
+	return err
 }
 
 func (t *translationRepo) FindOne(translationID domain.TranslationID) (domain.Translation, error) {
